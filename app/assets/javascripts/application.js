c213196200966bc6f9dc41940e82b43a98f10f6b
alimun88 <ayon88@hotmail.com>
2017-05-01 20:43:06 +0000

    Added admin users to chefs

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -14,7 +14,6 @@
 //= require jquery_ujs
 //= require twitter/bootstrap
 //= require bootstrap-sprockets
-//= require turbolinks
 //= require_tree .
 
 
--- db/schema.rb
+++ db/schema.rb
@@ -10,14 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170426055033) do
+ActiveRecord::Schema.define(version: 20170501052012) do
 
   create_table "chefs", force: :cascade do |t|
     t.string   "chef_name"
     t.string   "email"
-    t.datetime "created_at",      null: false
-    t.datetime "updated_at",      null: false
+    t.datetime "created_at",                      null: false
+    t.datetime "updated_at",                      null: false
     t.string   "password_digest"
+    t.boolean  "admin",           default: false
   end
 
   create_table "contacts", force: :cascade do |t|
