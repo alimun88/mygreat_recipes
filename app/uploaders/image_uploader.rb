bbcb3fb8d47a47416ff0ca26b3058044765d743a
alimun88 <ayon88@hotmail.com>
2017-05-29 19:52:01 +0000

    Updated S3bucket region

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -1,5 +1,8 @@
 
 class ImageUploader < CarrierWave::Uploader::Base
+  
+  include CarrierWave::MiniMagick
+  process resize_to_limit: [300, 300]
 
   # Include RMagick or MiniMagick support:
   # include CarrierWave::RMagick
